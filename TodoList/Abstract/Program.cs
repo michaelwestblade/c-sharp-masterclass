4f2601d1d2aed90954fb1463a82fed81109af860
michaelwestblade <michaelwestblade@gmail.com>
2024-08-13 13:09:51 -0500

    abstract classes and interfaces

---
+++ TodoList/Abstract/Program.cs
@@ -0,0 +1,78 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+namespace Coding.Exercise
+{
+    public static class ExerciseShapes
+    {
+        public static List<double> GetShapesAreas(List<Shape> shapes)
+        {
+            var result = new List<double>();
+            
+            foreach(var shape in shapes)
+            {
+                result.Add(shape.CalculateArea());
+            }
+            
+            return result;
+        }
+    }
+    
+    //your code goes here - define the Shape class
+    public abstract class Shape
+    {
+        public abstract double CalculateArea();
+    }
+    
+    public class Square : Shape
+    {
+        public double Side { get; }
+    
+        public Square(double side)
+        {
+            Side = side;
+        }
+        
+        //your code goes here
+        public override double CalculateArea()
+        {
+            return Side * Side;
+        }
+    }
+    
+    
+    public class Rectangle : Shape
+    {
+        public double Width { get; }
+        public double Height { get; }
+    
+        public Rectangle(double width, double height)
+        {
+            Width = width;
+            Height = height;
+        }
+        
+        //your code goes here
+        public override double CalculateArea()
+        {
+            return Width * Height;
+        }
+    }
+    
+    public class Circle : Shape
+    {
+        public double Radius { get; }
+    
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+        
+        //your code goes here
+        public override double CalculateArea()
+        {
+            return (Radius * Radius) * Math.PI;
+        }
+    }
+}
---
+++ TodoList/Interfaces/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Coding.Exercise
+{
+    public static class Exercise
+    {
+        public static int Transform(
+            int number)
+        {
+            var transformations = new List<INumericTransformation>
+            {
+                new By1Incrementer(),
+                new By2Multiplier(),
+                new ToPowerOf2Raiser()
+            };
+            
+            var result = number;
+            foreach(var transformation in transformations)
+            {
+                result = transformation.Transform(result);
+            }
+            return result;
+        }
+    }
+
+    public interface INumericTransformation
+    {
+        public int Transform(int input);
+    }
+
+    public class By1Incrementer : INumericTransformation
+    {
+        public int Transform(int input)
+        {
+            return input + 1;
+        }
+    }
+
+    public class By2Multiplier : INumericTransformation
+    {
+        public int Transform(int input)
+        {
+            return input * 2;
+        }
+    }
+
+    public class ToPowerOf2Raiser : INumericTransformation
+    {
+        public int Transform(int input)
+        {
+            return input * input;
+        }
+    }
+}
